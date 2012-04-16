8a862934fe28c74f83094f2716ea84226aa36194
Otto Hockel <hockel.otto@googlemail.com>
2012-04-16 13:43:13 +0200

    inital plugin

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='TracTestManager',
+      version=version,
+      description="TestManager Plugin for TRAC",
+      long_description="""\
+TestManager Plugin for TRAC""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='trac tests testcases',
+      author='Otto Hockel',
+      author_email='otto.hockel@inquant.de',
+      url='www.inquant.de',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
