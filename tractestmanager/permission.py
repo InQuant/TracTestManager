4d69050e86cee929301850b161fc9c90a02e442e
Otto Hockel <otto.hockel@inquant.de>
2012-05-08 18:23:23 +0200

    deleting obsolete files

--- tractestmanager/permission.py
+++
@@ -1,6 +0,0 @@
-from trac.core import Component, implements
-from trac.perm import IPermissionRequestor
-class MyPermissions(Component):
-    implements(IPermissionRequestor)
-    def get_permission_actions(self):
-        return ('TEST_MANAGER', 'TEST_CLIENT')
