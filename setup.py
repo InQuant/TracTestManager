5bdf354495e1de2957a3edf78e278fd3c5ff3081
Otto Hockel <otto.hockel@inquant.de>
2012-05-10 22:35:25 +0200

    updating setup cfg and py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='TracTestManager',
       version=version,
       description="TestManager Plugin for TRAC",
       long_description="""\
-TestManager Plugin for TRAC""",
+      TestManager Plugin for TRAC""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='trac tests testcases',
       author='Otto Hockel',
@@ -16,6 +16,7 @@ TestManager Plugin for TRAC""",
       url='www.inquant.de',
       license='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      package_data={'tractestmanager' : ['templates/*.html', 'htdocs/js/*.js', 'htdocs/css/*.css']},
       include_package_data=True,
       zip_safe=False,
       install_requires=[
