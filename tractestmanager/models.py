8a6680d69d15ddda6aa37dd0d925ce961623b5a3
Otto Hockel <hockel.otto@googlemail.com>
2012-05-11 16:09:37 +0200

    building model class for testactions and testcases

--- tractestmanager/models.py
+++ tractestmanager/models.py
@@ -17,54 +17,81 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-from elixir import *
-import datetime
+#from elixir import *
+#import datetime
+
+import trac.db
+from trac.db import with_transaction
 
 __author__ = 'Otto Hockel <otto.hockel@inquant.de>'
 __docformat__ = 'plaintext'
 
-metadata.bind = "sqlite:///testmanager.sqlite"
-metadata.bind.echo = True
+#metadata.bind = "sqlite:///testmanager.sqlite"
+#metadata.bind.echo = True
 
-class Testaction(Entity):
+class TracDBModel(object):
+    """ Generic DB Model
+    """
+
+    def __init__(self, env):
+        self.env = env
+        self.db  = env.get_db_cnx()
+        self.cursor = self.db.cursor()
+
+    def __repr__(self):
+        return "<TestManager:TracDBModel>"
+
+class Testaction(dict):
     """ Testaction model
         an action is part of a testcase
     """
 
-    id       = Field(Integer, primary_key=True)
-    title    = Field(UnicodeText)
-    desc     = Field(UnicodeText)
-    result   = Field(UnicodeText)
-    testcase = ManyToOne('Testcase')
+    #id       = Field(Integer, primary_key=True)
+    #title    = Field(UnicodeText)
+    #desc     = Field(UnicodeText)
+    #result   = Field(UnicodeText)
+    #testcase = ManyToOne('Testcase')
 
     def __repr__(self):
         return '<Testaction: "%s">' % (self.title)
 
-class Testcase(Entity):
+class Testcase(TracDBModel):
     """ Testcase model
     """
 
-    id        = Field(Integer, primary_key=True)
-    wiki      = Field(Unicode(128))
-    title     = Field(UnicodeText)
-    tester    = Field(Unicode(128))
-    actions   = OneToMany('Testaction')
-    testcases = ManyToOne('Testrun')
+    #id        = Field(Integer, primary_key=True)
+    id = None
+    #wiki      = Field(Unicode(128))
+    wiki = None
+    #title     = Field(UnicodeText)
+    title = None
+    #tester    = Field(Unicode(128))
+    tester = None
+    #actions   = OneToMany('Testaction')
+    actions = list()
+    #testcases = ManyToOne('Testrun')
+    testrun = None
 
     def __repr__(self):
         return '<Testcase: "%s">' % (self.title)
 
-class Testrun(Entity):
+class Testrun(dict):
     """ Testrun model
         a Testplan is expected to be a wiki page
     """
 
-    id              = Field(Integer, primary_key=True)
-    start           = Field(DateTime, default=datetime.datetime.now)
-    end             = Field(DateTime)
-    config          = Field(UnicodeText)
-    testcases       = OneToMany('Testcase')
+    #id              = Field(Integer, primary_key=True)
+    id = None
+    #start           = Field(DateTime, default=datetime.datetime.now)
+    start = None
+    #end             = Field(DateTime)
+    end = None
+    #config          = Field(UnicodeText)
+    config = None
+    #testcases       = OneToMany('Testcase')
+    testcases = list()
 
     def __repr__(self):
         return '<Testrun: started on "%s">' % (self.start)
 
+# vim: set ft=python ts=4 sw=4 expandtab :
