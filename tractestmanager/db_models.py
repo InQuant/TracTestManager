db30ed023c596b0e2821a390e5844b4310a2efe1
lmende <lutz.mende@inquant.de>
2012-05-20 15:11:53 +0200

    database class implemented and doctested

--- tractestmanager/db_models.py
+++ tractestmanager/db_models.py
@@ -1,233 +1,297 @@
 # -*- coding: utf-8 -*-
 #
-# File: TestPlan.py
+# File: db_models.py - wraps the trac sql stuff needed in testman
 #
 # Copyright (c) Inquant GmbH
 #
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-from trac.core import Component
-import trac.db
-from trac.db import with_transaction
-
-__author__ = 'Otto Hockel <otto.hockel@inquant.de>'
+__author__ = 'Lutz Mende <lutz.mende@inquant.de>'
 __docformat__ = 'plaintext'
 
-#def initenv(_env):
-    #global ENV
-    #ENV = _env
+from trac.core import Component
+from trac.db import with_transaction
+import trac.db
 
+import logging
+import string
+
+LOGGER="db_models"
+def debug(msg): logging.getLogger(LOGGER).debug(msg)
+
+# modul exceptions
 class DbException(Exception):
     pass
 class DbAlreadyExistException(DbException):
     pass
 
-class DbTestCases(Component):
-    """ Wrap the database to query TestCases
+# (case | action) status
+NOT_TESTED= 'not tested'
+PASSED= 'passed'
+PASSED_COMMENT= 'passed with comment'
+FAILED= 'failed'
+
+# TestCase collumns
+TC_KEYS= [ 'tcid', 'wiki', 'title', 'description', 'version', 'tester', 'testrun', 'status']
+# TestAction collumns
+TA_KEYS= ['testrun', 'tcid', 'title', 'description', 'expected_result', 'status', 'comment']
+
+##############################################################################
+def orderedValues(keys, adict):
     """
-
-    def get(self):
-        @with_transaction(self.env)
-        def _get(self, db, **kwargs):
-            cursor = db.cursor()
-            stmt = ("SELECT wiki,title,revision,tester,testrun,status"
-                           "FROM testcases "
-                           "WHERE")
-            for key, value in dict(KeyValues):
-                stmt = ("%s AND %s=%s" % (stmt, key, value))
-            cursor.execute(stmt)
-            row = cursor.fetchone()
-            pass
-
-    def tcCreate(self, **kwargs):
-        @with_transaction(self.env)
-        def _tcCreate(db, kwargs):
-            cursor = db.cursor()
-            cursor.execute("""INSERT INTO testcases 
-                (wiki,title,revision,tester,testrun,status)
-                VALUES (%(wiki)s,%(title)s,%(revision)s,%(tester)s,%(testrun)s,%(status)s)""" % kwargs)
-
-class TracDBModel(object):
-    """ Generic DB Model
-    """
-
-    def __init__(self, env):
-        self.env = env
-        #self.db  = env.get_db_cnx()
-        #self.cursor = self.db.cursor()
-
-    def __repr__(self):
-        return "<TestManager:TracDBModel>"
-
-class Testcase(TracDBModel):
-    """ Testcase model
-    """
-
-    id       = None
-    wiki     = None
-    title    = None
-    # the wiki page revision to take
-    revision = None
-    tester   = None
-    # one testcase has one or more actions
-    actions  = list()
-    # one or more testcases belong to one testrun
-    testrun  = None
-    status   = None
-
-    # a Testcase can be initialized with an existing id.
-    # if the id exists, the full object will be loaded
-
-    def __init__(self, env, id=None):
-        if id is not None:
-            super(Testaction, self).__init__(env)
-            # TODO: exception handling if testaction not in db
-            self.id = id
-            self._fetch(id, self.db)
-        else:
-            # TODO: not implemented yet, for insertion purpose
-            pass
-
-    def __repr__(self):
-        return '<Testcase: "%s">' % (self.title)
-
-    def _fetch(self, id, db=None):
-        if not db:
-            db = self.env.get_db_cnx()
-        cursor = db.cursor()
-        cursor.execute("SELECT wiki,title,revision,tester,testrun,status"
-                       "FROM testcases "
-                       "WHERE id=%s", int(id))
-        row = cursor.fetchone()
-        # if not row -> testcase not available
-        if row:
-            wiki, title, revision, tester, testrun, status = row
-            self.wiki     = wiki
-            self.title    = title
-            self.revision = revision
-            self.tester   = tester
-            self.testrun  = testrun
-            self.status   = status
-            self.id       = id
-            # Fetch all testactions for this testcase
-            # "SELECT * FROM testactions WHERE testcase_id=%s", int(id)
-            # for row in cursor.fetchall(): testactions.append(row)
-            #pass
-
-    def get_actions(self, db=None):
-        actions = list()
-        @self.env.with_transaction(db)
-        def do_load_actions(db):
-            cursor = db.cursor()
-            cursor.execute("SELECT id,description,expected_result"
-                           "FROM testactions "
-                           "WHERE testcase=", int(self.id))
-            rows = cursor.fetchall()
-            for row in rows:
-                actions.append(row)
-        # return Testactions
-        return actions
-
-    def save(self, db=None):
-        @self.env.with_transaction(db)
-        def do_save(db):
-            cursor = db.cursor()
-            cursor.execute("""
-                    INSERT INTO testcases (wiki,title,revision,tester,
-                                           actions,testrun,status)
-                    VALUES (%s,%s,%s,%s,%s,%s)
-                    """, (self.wiki, self.title, self.revision, self.tester,
-                        self.testrun, self.status))
-
-    def delete(self, db=None):
-        @self.env.with_transaction(db)
-        def do_save(db):
-            pass
-        pass
-
-    def check_integrity(self):
-        # TODO: check integrity of the whole testaction
-        #       return errors and things like this
-        return True
-
-class Testaction(TracDBModel):
-    """ Testaction model
-        an action is part of a testcase
-    """
-
-    id              = None
-    # one or more testactions belong to one testcase
-    testcase        = None
-    # the comment is appended to the testrun. for the
-    # purpose of integrity, we need the action as base
-    comment         = None
-    status          = None
-    description     = None
-    expected_result = None
-
-    # a Testaction can be initialized with an existing id.
-    # if the id exists, the full object will be loaded
-
-    def __init__(self,env,id=None):
-        if id is not None:
-            super(Testaction, self).__init__(env)
-            # TODO: exception handling if testaction not in db
-            self.id = id
-            self._fetch(id, self.db)
-        else:
-            # TODO: not implemented yet, for insertion purpose
-            pass
-
-    def _fetch(self, id, db=None):
-        if not db:
-            db = self.env.get_db_cnx()
-        cursor = db.cursor()
-        cursor.execute("SELECT testcase,comment,status,description,expected_result"
-                       "FROM testactions "
-                       "WHERE id=%s", int(id))
-        row = cursor.fetchone()
-        # if not row -> testcase not available
-        if row:
-            testcase, comment, status, description, expected_result = row
-            self.testcase = testcase
-            self.comment = comment
-            self.status = status
-            self.description = description
-            self.expected_result = expected_result
-            self.id = id
-
-    def __repr__(self):
-        return '<Testaction: "%s">' % (self.title)
-
-    def save(self, db=None):
-        pass
-
-    def delete(self, db=None):
-        pass
-
-    def getXML(self, id, db=None):
-        pass
-
-    def check_integrity(self):
-        # TODO: check integrity of the whole testaction
-        #       return errors and things like this
-        return True
-
-class DbTestcases(object):
+    Constructs the value list for insert operations in the right order.
     
-    def get(self, kwargs):
-        pass
+    >>> keys= ['a', 'b', 'c']
+    >>> adict= {'c':3, 'a':1, 'b':2}
+    >>> orderedValues(keys, adict)
+    [1, 2, 3]
+
+    """
+    return [adict.get(k, None) for k in keys]
+
+##############################################################################
+def getInsertPlaceHolders(keys):
+    """
+    Builds the tracdb required '%s' place holder list, 
+    e.g.: the '%s, %s, %s, %s' string for 4 keys
+
+    >>> keys= ['a', 'b', 'c']
+    >>> getInsertPlaceHolders(keys)
+    '%s, %s, %s'
+
+    """
+    return string.join(['%s' for i in range(len(keys))], ', ')
+
+##############################################################################
+#class DbLite(Component):
+class DbLite(object):
+    """
+    Class to wrap the SQL stuff about TestCases and TestActions ...
+
+    >>> from trac.env import Environment
+    >>> env= Environment( '/Users/lmende/Dropbox/projekte/BoschRexroth/TracTestManager/testman' )
+    
+    1. Instanciate the class and set it up.
+
+    >>> db= DbLite(env)
+
+    >>> db.setup()
+
+    2. Build a dict for a Testcase to add.
+
+    >>> tcvals= ['TcDocCreate', 'Create Docs', None, 2, 'lmende', 3, NOT_TESTED]
+
+    3. Build a list of dicts for TestActions to add.
+
+    >>> vals1= [ 3, None, 'Add to Workspace', 'Add to WS desc', 'Docs added',PASSED, None ]
+    >>> vals2= [ 3, None, 'Checkout', 'Checkout desc', 'Docs checked out', NOT_TESTED, None ]
+    >>> actions= [dict(zip(TA_KEYS, vals1)), dict(zip(TA_KEYS, vals2))]
+    
+    4. Insert the test case and actions.
+
+    >>> db.insertTestCase( dict(zip(TC_KEYS[1:], tcvals)), actions )
+
+    Get all testcases
+    >>> db.getTestCases()
+    [...]
+
+    Get filtered testcases
+    >>> db.getTestCases(status= NOT_TESTED)
+    [...]
+
+    Get all actions
+    >>> db.getTestActions()
+    [...]
+
+    Get filtered actions
+    >>> db.getTestActions(testrun= 3, status= NOT_TESTED)
+    [...]
+
+    """
+
+    ##########################################################################
+    def __init__(self, _env= None):
+        debug('DbLite.__init__()')
+        if _env: self.env= _env
+
+    ##########################################################################
+    def setup(self):
+        """
+        Sets up the two additional testman tables
+        """
+        debug('DbLite.setup()')
+
+        @with_transaction(self.env)
+        def _createTables(db):
+            c= db.cursor()
+
+            # create testcases table
+            cols= string.join([
+               'tcid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT',
+               'wiki VARCHAR(512) NOT NULL',
+               'title VARCHAR(512) NOT NULL',
+               'description TEXT',
+               'version INTEGER NOT NULL',
+               'tester VARCHAR(80)',
+               'testrun INTEGER NOT NULL',
+               'status VARCHAR(80) NOT NULL',
+                    ], ', ')
+
+            stmt= """CREATE TABLE IF NOT EXISTS testcases (%s)""" % cols
+            debug(stmt)
+            c.execute(stmt)
+
+            # create testactions table
+            cols= string.join([
+               'testrun INTEGER NOT NULL',
+               'tcid INTEGER NOT NULL REFERENCES testcases (tcid)',
+               'title VARCHAR(512) NOT NULL',
+               'description TEXT',
+               'expected_result TEXT NOT NULL',
+               'status VARCHAR(80) NOT NULL',
+               'comment TEXT',
+                    ], ', ')
+
+            stmt= """CREATE TABLE IF NOT EXISTS testactions (%s, PRIMARY KEY
+            (testrun, tcid, title))""" % cols
+            debug(stmt)
+            c.execute(stmt)
+
+    ##########################################################################
+    def insertTestCase(self, tcDict, actionDicts= None):
+        """
+        inserts one new TestCase into the table(s)
+
+        tcDict - Dictionary with tescase keys, values
+        actionDicts - List of dictionaries with action keys, values
+            tcid can be left.
+
+        returns new testcase id
+        """
+        debug('DbLite.insertTestCase()')
+        
+        @with_transaction(self.env)
+        def _insertTestCase(db):
+            c= db.cursor()
+
+            # constructs a statement of form "... VALUES (%s,%s,%s,%s)"
+            # w/o autovalue tcid
+            stmt= "INSERT INTO testcases (%s) VALUES (%s)" % (
+                    string.join(TC_KEYS[1:], ',') , 
+                    getInsertPlaceHolders(TC_KEYS[1:]))
+            debug(stmt)
+            
+            c.execute(stmt, orderedValues(TC_KEYS[1:], tcDict))
+            tcid= c.lastrowid
+            debug("tcid= %d" % tcid)
+
+            if (actionDicts):
+                for ad in actionDicts: ad['tcid']= tcid
+
+                # constructs a statement of form "... VALUES (%s,%s,%s,%s)"
+                stmt= "INSERT INTO testactions (%s) VALUES (%s)" % (
+                    string.join(TA_KEYS, ',') , 
+                    getInsertPlaceHolders(TA_KEYS))
+                debug(stmt)
+                
+                # build list from action value lists
+                actions= []
+                for d in actionDicts:
+                    actions.append( orderedValues(TA_KEYS, d))
+
+                # insert all actions in one step
+                c.executemany(stmt, actions)
+
+    ##########################################################################
+    def getTestCaseCollumns(self):
+        return TC_KEYS
+
+    ##########################################################################
+    def getTestCases(self, testrun= None, status= None):
+        """
+        selects all testcases of a given testrun with a given status.
+        """
+        debug('DbLite.getTestCases()')
+        rows= []
+
+        @with_transaction(self.env)
+        def _getTestCases(db):
+            c= db.cursor()
+            
+            # build filter
+            filters= []
+            fvalues= []
+            if testrun: 
+                filters.append( 'testrun=%s' )
+                fvalues.append( testrun )
+            if status: 
+                filters.append( 'status=%s' )
+                fvalues.append( status )
+
+            # build statement
+            stmt= "SELECT * FROM testcases"
+            if filters:
+                 stmt= stmt + ' WHERE ' + string.join( filters, ' AND ')
+            
+            debug( stmt )
+            c.execute( stmt, fvalues )
+            rows= c.fetchall()
+            debug(rows)
+            return rows
+        return rows
+
+    ##########################################################################
+    def getTestActionCollumns(self):
+        return TC_ACTIONS
+
+    ##########################################################################
+    def getTestActions(self, testrun= None, tcid= None, status= None):
+        """
+        Selects all testactions of a given testrun and testcase id with a given
+        status.
+        """
+        debug('DbLite.getTestActions()')
+        rows= []
+
+        @with_transaction(self.env)
+        def _getTestActions(db):
+            c= db.cursor()
+            
+            # build filter
+            filters= []
+            fvalues= []
+            if testrun: 
+                filters.append( 'testrun=%s' )
+                fvalues.append( testrun )
+            if tcid: 
+                filters.append( 'tcid=%s' )
+                fvalues.append( tcid )
+            if status: 
+                filters.append( 'status=%s' )
+                fvalues.append( status )
+
+            # build statement
+            stmt= "SELECT * FROM testactions"
+            if filters:
+                 stmt= stmt + ' WHERE ' + string.join( filters, ' AND ' )
+            
+            debug( stmt )
+            c.execute( stmt, fvalues )
+            rows= c.fetchall()
+            debug(rows)
+            return rows
+        return rows
+
+##############################################################################
+if __name__ == "__main__":
+    logging.basicConfig(
+            level= logging.DEBUG, 
+            #level= logging.INFO, 
+            #format= "%(asctime)s %(levelname)s %(name)s %(message)s",
+            #format= "%(asctime)s %(levelname)s %(name)s %(message)s",
+            format= "%(message)s",
+            #datefmt= "%Y-%m-%d %H:%M:%S",
+            )
+    import doctest
+    doctest.testmod(optionflags= doctest.ELLIPSIS)
 
 # vim: set ft=python ts=4 sw=4 expandtab :
