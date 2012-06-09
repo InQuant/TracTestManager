9fb671e0edf39c249a9a31cd2580995bef41783c
Otto Hockel <hockel.otto@googlemail.com>
2012-06-09 21:04:06 +0200

    moving deps from lib to models

--- tractestmanager/TestManagerLib.py
+++ tractestmanager/TestManagerLib.py
@@ -19,33 +19,11 @@
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 from trac.wiki import WikiSystem
-from trac.ticket.query import Query as TicketQuery
-from trac.ticket.model import Ticket
 
 def _get_wiki_pages(self,prefix):
     """ wrap the wiki api
     """
     for page in WikiSystem(self.env).get_pages(prefix):
         yield page
-
-def add_testrun(env, config, user, macro_text):
-    """ add new testrun ticket
-    """
-    data = dict()
-    data['owner'] = user
-    data['reporter'] = user
-    data['summary'] = config['id']
-    data['description'] = macro_text
-    data['type'] = 'testrun'
-    data['status'] = 'accepted'
-    t = Ticket(env)
-    t.populate(data)
-    return t.insert()
-
-def defect_testrun(env, id, error_message):
-    t = Ticket(env, id)
-    t['status'] = 'new'
-    t['description'] += error_message
-    return t.save_changes()
 
 # vim: set ft=python ts=4 sw=4 expandtab :
