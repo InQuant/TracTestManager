ce4c232896e61f1c1af6323cfabcc3f68c69916f
Otto Hockel <hockel.otto@googlemail.com>
2012-06-22 19:45:50 +0200

    refactoring whole accordion to be more modular, full operations are now supportet including callback functions and loading of status

--- tractestmanager/htdocs/js/accordion.js
+++ tractestmanager/htdocs/js/accordion.js
@@ -1,27 +1,68 @@
 jQuery(document).ready(function($) {
 
+
+    $('input:button').click(function(){
+        var actionid = parseInt(this.parentElement.id);
+        set_status(actionid, this.value);
+        var trig = $(this.parentElement.previousElementSibling);
+        var duration = 100;
+        // close old container
+        if ( trig.hasClass('trigger_active') ) {
+            trig.next('.toggle_container').slideToggle(duration);
+        } else {
+            $('.trigger_active').next('.toggle_container').slideToggle(duration);
+            trig.next('.toggle_container').slideToggle(duration);
+        };
+        if($(this.parentElement.parentElement.parentElement.nextElementSibling) != null){
+            var trig = $(this.parentElement.parentElement.parentElement.nextElementSibling.children[0].children[1]);
+            if (trig != null){
+                // the next one is opened, only close me
+                if ( trig.next('.toggle_container')[0].style.display != "block" ){
+                    trig.next('.toggle_container').slideToggle(duration);
+                };
+            };
+        };
+        return false;
+    });
+
     // Accordion
     $('.trigger').not('.trigger_active').next('.toggle_container').hide();
-    // show the first container - buggy!
-    // $("[class^=toggle_container]:first").show();
+    $("[class^=trigger]:first").next('.toggle_container').slideToggle(0);
     
     $('.trigger').click( function() {
         var trig = $(this);
         var duration = 100;
         if ( trig.hasClass('trigger_active') ) {
             trig.next('.toggle_container').slideToggle(duration);
-            trig.removeClass('trigger_active');
-            trig.css("background","white");                
         } else {
             $('.trigger_active').next('.toggle_container').slideToggle(duration);
-            $('.trigger_active').removeClass('trigger_active');                
             trig.next('.toggle_container').slideToggle(duration);
-            trig.css("background","#eee");
-            trig.addClass('trigger_active');
         };
         return false;
     });                       
 });
+
+var set_status = function($id, $value){
+    var inputs = $("#accordion" + $id + ' :input');
+    var values = {};
+    inputs.each(function(){
+        values[this.name] = $(this).val();
+    });
+    t_area            = $('#accordion'+values['action']).find('textarea')[0];
+    if(t_area.value && $value == "passed"){
+        values['comment'] = t_area.value;
+        values['status']  = "passed with comment";
+    }
+    else{
+        values['status']  = $value;
+    }
+    url               = getBaseURL()+'trac/json_testaction';
+    // do the post request
+    $.post(url, values, function(data){
+        json = jQuery.parseJSON(data);
+        $("#accordion" + $id)[0].children[1].style['background'] = status_color[json.status];
+    });
+};
 
 function getBaseURL() {
     var url = location.href;  // entire url including querystring - also: window.location.href;
@@ -45,20 +86,3 @@ function getBaseURL() {
 
 }
 
-var set_status = function($id, $value){
-    var inputs = $("#accordion" + $id + ' :input');
-    var values = {};
-    inputs.each(function(){
-        values[this.name] = $(this).val();
-    });
-    t_area            = $('#accordion'+values['action']).find('textarea')[0];
-    if(t_area.value){
-        values['comment'] = t_area.value;
-    }
-    values['status']  = $value;
-    url               = getBaseURL()+'trac/json_testaction';
-    // do the post request
-    $.post(url, values, function(data){
-        console.log(data)
-    });
-};
