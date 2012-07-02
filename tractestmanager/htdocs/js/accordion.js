2e792e562cc862f26bf93e9c99ecc7d6835664f2
Hans-Peter Locher <hans-peter.locher@inquant.de>
2012-07-02 14:31:35 +0200

    fix url to json_testaction

--- tractestmanager/htdocs/js/accordion.js
+++ tractestmanager/htdocs/js/accordion.js
@@ -56,33 +56,10 @@ var set_status = function($id, $value){
     else{
         values['status']  = $value;
     }
-    url               = getBaseURL()+'trac/json_testaction';
+    url               = '../../../json_testaction';
     // do the post request
     $.post(url, values, function(data){
         json = jQuery.parseJSON(data);
         $("#accordion" + $id)[0].children[1].style['background'] = status_color[json.status];
     });
 };
-
-function getBaseURL() {
-    var url = location.href;  // entire url including querystring - also: window.location.href;
-    var baseURL = url.substring(0, url.indexOf('/', 14));
-
-
-    if (baseURL.indexOf('http://localhost') != -1) {
-        // Base Url for localhost
-        var url = location.href;  // window.location.href;
-        var pathname = location.pathname;  // window.location.pathname;
-        var index1 = url.indexOf(pathname);
-        var index2 = url.indexOf("/", index1 + 1);
-        var baseLocalUrl = url.substr(0, index2);
-
-        return baseLocalUrl + "/";
-    }
-    else {
-        // Root Url for domain name
-        return baseURL + "/";
-    }
-
-}
-
