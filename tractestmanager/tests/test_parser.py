772dcc6c87ad2813ce6d7229d7dd91e6b1dd3e08
rain0r <rainer@hihn.org>
2012-11-22 10:58:47 +0100

    added Test for parsing a WikiPage

--- tractestmanager/tests/test_parser.py
+++ tractestmanager/tests/test_parser.py
@@ -1,7 +1,8 @@
 import pytest
 from trac.core import TracError
-from trac_pytest import build_trac_env
+#from trac_pytest import build_trac_env
 from tractestmanager import TestcaseParser
+from trac.wiki import WikiPage
 
 def test_parseTestcase_NotExistingWikiPage(build_trac_env):
     env = build_trac_env
@@ -10,3 +11,16 @@ def test_parseTestcase_NotExistingWikiPage(build_trac_env):
         # a Wiki Page that doenst exist
         tcp.parseTestcase('NotExistingWikiPage')
 
+def test_parseTestcase_parseWikiPage(build_trac_env):
+    env = build_trac_env
+
+    f = open("src/TracTestManager/docs/wiki/TcTestplanCreate")
+
+    wikipage = WikiPage(env, 'TcTestplanCreate')
+    wikipage.text = f.read().decode('utf-8')
+    wikipage.save("pytest", "comment", "127.0.0.1")
+
+    tcp = TestcaseParser(env)
+    tcp.parseTestcase(pagename='TcTestplanCreate')
+
+    assert 1
